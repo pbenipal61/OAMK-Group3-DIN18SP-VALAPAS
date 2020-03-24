90d294bb07924ee7b6e8090554e8b4a2f9d3f828
Alexandru Grosu <alex.gm98@yahoo.com>
2020-03-24 20:50:22 +0200

    Made Customer Profile Activity + minor changes

---
+++ app/Valapas/app/src/main/java/com/raulbrumar/valapas/Activities/Customers/CustomerProfileActivity.java
@@ -0,0 +1,18 @@
+package com.raulbrumar.valapas.Activities.Customers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.raulbrumar.valapas.R;
+
+public class CustomerProfileActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_profile);
+    }
+}
