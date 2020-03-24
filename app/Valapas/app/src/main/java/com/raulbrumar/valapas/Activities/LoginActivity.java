7087780409dc221cb4303833c549378aeeaaef9e
Raul Brumar <raulbrumar@yahoo.de>
2020-03-24 15:35:31 +0200

    Created project and part of the UI

---
+++ app/Valapas/app/src/main/java/com/raulbrumar/valapas/Activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.raulbrumar.valapas.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.raulbrumar.valapas.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
