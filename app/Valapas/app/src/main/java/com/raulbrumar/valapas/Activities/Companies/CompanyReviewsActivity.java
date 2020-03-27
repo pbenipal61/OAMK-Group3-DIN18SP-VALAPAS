6f98f062d9e0c8d5c2de99018bfda5ff608e1f43
Raul Brumar <raulbrumar@yahoo.de>
2020-03-27 19:20:30 +0200

    Added activities for company side

---
+++ app/Valapas/app/src/main/java/com/raulbrumar/valapas/Activities/Companies/CompanyReviewsActivity.java
@@ -0,0 +1,16 @@
+package com.raulbrumar.valapas.Activities.Companies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.raulbrumar.valapas.R;
+
+public class CompanyReviewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_company_reviews);
+    }
+}
---
+++ app/Valapas/app/src/main/java/com/raulbrumar/valapas/Activities/Companies/CompanyUpdateInfo.java
@@ -0,0 +1,16 @@
+package com.raulbrumar.valapas.Activities.Companies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.raulbrumar.valapas.R;
+
+public class CompanyUpdateInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_company_update_info);
+    }
+}
