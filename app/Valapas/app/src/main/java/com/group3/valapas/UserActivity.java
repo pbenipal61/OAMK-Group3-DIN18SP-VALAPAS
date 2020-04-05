aa5347f5baca8ebf5419813448c9656f2def147f
k9lada00 <60172591+k9lada00@users.noreply.github.com>
2020-04-05 16:42:23 +0300

    Changed Content View

--- app/Valapas/app/src/main/java/com/group3/valapas/UserActivity.java
+++ app/Valapas/app/src/main/java/com/group3/valapas/UserActivity.java
@@ -28,7 +28,7 @@ public class UserActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_browse);
+        setContentView(R.layout.activity_user);
 
         mValapasStatePagerAdapter = new ValapasStatePagerAdapter(getSupportFragmentManager());
         mViewPager = (ViewPager) findViewById(R.id.browsing);
