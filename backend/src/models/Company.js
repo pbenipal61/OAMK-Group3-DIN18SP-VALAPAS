13a2f790a828a630f6b2ac8fcc50eb23d2fe8f2f
Prabhjot Benipal <t8sipr00@students.oamk.fi>
2020-04-07 21:17:47 +0300

    Turned companies into a user type

--- backend/src/models/Company.js
+++ backend/src/models/Company.js
@@ -5,6 +5,14 @@ const schema = {
         type: String,
         required: true
     },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
     description: {
         type: String,
         required: true
