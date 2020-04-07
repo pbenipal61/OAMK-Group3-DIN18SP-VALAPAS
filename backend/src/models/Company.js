3c686c5a4762a54d85810e2c5ef4e4122c9e3c30
Prabhjot Benipal <t8sipr00@students.oamk.fi>
2020-04-07 20:03:11 +0300

    Removed userId for company creation

--- backend/src/models/Company.js
+++ backend/src/models/Company.js
@@ -1,11 +1,6 @@
 import mongoose from 'mongoose';
 
 const schema = {
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'User'
-    },
     name: {
         type: String,
         required: true
