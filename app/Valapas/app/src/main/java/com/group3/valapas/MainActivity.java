e1930bd900eaedc417cbc01a7ae1baef25daac24
k9lada00 <60172591+k9lada00@users.noreply.github.com>
2020-04-08 15:17:18 +0300

    Eliminating Fragments

--- app/Valapas/app/src/main/java/com/group3/valapas/MainActivity.java
+++ app/Valapas/app/src/main/java/com/group3/valapas/MainActivity.java
@@ -1,15 +1,10 @@
 package com.group3.valapas;
 
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
-import com.group3.valapas.ValapasStatePagerAdapter;
-
-import com.group3.valapas.Activities.Customers.UserBrowseTab;
 
 
 public class MainActivity extends AppCompatActivity
@@ -19,24 +14,5 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void selectSignIn(View view)
-    {
-        Toast.makeText(this, "Signing In...", Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(this, UserBrowseTab.class);
-        startActivity(intent);
-    }
-
-    public void selectSignUp(View view)
-    {
-        Intent intent = new Intent(this, SignUpActivity.class);
-        startActivity(intent);
-    }
-
-    public void selectBrowse(View view)
-    {
-        Intent intent = new Intent(this, BrowseActivity.class);
-        startActivity(intent);
     }
 }
