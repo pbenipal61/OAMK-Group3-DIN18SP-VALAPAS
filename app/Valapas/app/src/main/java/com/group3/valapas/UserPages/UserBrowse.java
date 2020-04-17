b1eec8a722b32af02d6a3eaf43242f32870c3931
Alexandru Grosu <alex.gm98@yahoo.com>
2020-04-17 20:39:11 +0300

    Fixed UI and added recyclerView for categories

--- app/Valapas/app/src/main/java/com/group3/valapas/UserPages/UserBrowse.java
+++ app/Valapas/app/src/main/java/com/group3/valapas/UserPages/UserBrowse.java
@@ -1,19 +1,72 @@
 package com.group3.valapas.UserPages;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.group3.valapas.Adapters.CategoriesAdapter;
 import com.group3.valapas.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class UserBrowse extends AppCompatActivity
 {
+    private RecyclerView categoriesView;
+    private RecyclerView.LayoutManager layoutManager;
+    private RecyclerView.Adapter categoriesAdapter;
+
+    private EditText searchText;
+
+    // the array of categories (might have a class later)
+    private ArrayList<String> categories = new ArrayList<>();
+
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_browse);
+
+        context = this;
+
+        categories = new ArrayList<>(Arrays.asList("Restaurants", "Fast Food", "Sports", "Leisure", "Entertainment", "VR"));
+
+        categoriesView = findViewById(R.id.categoriesView);
+        categoriesView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        categoriesView.setLayoutManager(layoutManager);
+        categoriesAdapter = new CategoriesAdapter(categories);
+        categoriesView.setAdapter(categoriesAdapter);
+
+        searchText = findViewById(R.id.searchText);
+        searchText.setOnEditorActionListener(new TextView.OnEditorActionListener()
+        {
+
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH)
+                {
+                    performSearch();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     public void selectProfile(View v)
@@ -34,24 +87,11 @@ public class UserBrowse extends AppCompatActivity
         startActivity(i);
     }
 
-    public void selectCategory(View v)
+    private void performSearch()
     {
-        Intent i = new Intent (this, UserBrowseCategory.class);
-        startActivity(i);
-        finish();
+        searchText.clearFocus();
+        InputMethodManager in = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        in.hideSoftInputFromWindow(searchText.getWindowToken(), 0);
     }
 
-    public void selectPopular(View v)
-    {
-        Intent i = new Intent (this, UserBrowsePopular.class);
-        startActivity(i);
-        finish();
-    }
-
-    public void selectPrice(View v)
-    {
-        Intent i = new Intent (this, UserBrowsePrice.class);
-        startActivity(i);
-        finish();
-    }
 }
