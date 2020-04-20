94e4433eacc2a173253d31c9334d78b8f8ec7ef6
Raul Brumar <raulbrumar@yahoo.de>
2020-04-20 14:51:38 +0300

    Updated UI on the Company side

--- app/Valapas/app/src/main/java/com/group3/valapas/CompanyPages/CompanyUpdateInfo.java
+++ app/Valapas/app/src/main/java/com/group3/valapas/CompanyPages/CompanyUpdateInfo.java
@@ -2,7 +2,9 @@ package com.group3.valapas.CompanyPages;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.group3.valapas.R;
 
@@ -12,5 +14,29 @@ public class CompanyUpdateInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_company_update_info);
+    }
+
+    public void onHomeClick(View view)
+    {
+        Intent intent = new Intent(this, CompanyHomeActivity.class);
+        startActivity(intent);
+    }
+
+    public void onUpdateInfoClick(View view)
+    {
+        Intent intent = new Intent(this, CompanyUpdateInfo.class);
+        startActivity(intent);
+    }
+
+    public void onReviewsClick(View view)
+    {
+        Intent intent = new Intent(this, CompanyReviewsActivity.class);
+        startActivity(intent);
+    }
+
+    public void onBookingsClick(View view)
+    {
+        Intent intent = new Intent(this, CompanyBookingsActivity.class);
+        startActivity(intent);
     }
 }
