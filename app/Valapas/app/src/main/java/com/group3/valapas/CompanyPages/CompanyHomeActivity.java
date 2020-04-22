88b6c930d0f6552cc5599e00c1f6a0fed6d6cfe3
Raul Brumar <raulbrumar@yahoo.de>
2020-04-22 17:20:24 +0300

    small company home changes

--- app/Valapas/app/src/main/java/com/group3/valapas/CompanyPages/CompanyHomeActivity.java
+++ app/Valapas/app/src/main/java/com/group3/valapas/CompanyPages/CompanyHomeActivity.java
@@ -3,17 +3,44 @@ package com.group3.valapas.CompanyPages;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.group3.valapas.ApiHandler.ApiHandler;
+import com.group3.valapas.Models.Company;
+import com.group3.valapas.Models.CompanyBuilder;
 import com.group3.valapas.R;
 
+import org.w3c.dom.Text;
+
 public class CompanyHomeActivity extends AppCompatActivity {
+
+    private TextView companyNameTextView;
+    private TextView companyDetailsTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_company_home);
+
+        Company company = new CompanyBuilder().buildCompany(ApiHandler.getBearerToken());
+
+        companyNameTextView = findViewById(R.id.name);
+        companyDetailsTextView = findViewById(R.id.details);
+
+
+        String details = "";
+        details += "Address: " + company.getAddress() + "\n";
+        details += "Postal code: " + company.getPostalCode() + "\n";
+        details += "City: " + company.getCity() + "\n";
+        details += "Country: " + company.getCountry() + "\n";
+        details += "Description: " + company.getDescription() + "\n";
+        details += "Categories: " + "sdfsddsf"/*categoriesString*/ + "\n";
+        details += "Email: " + company.getEmail() + "\n";
+
+        companyNameTextView.setText(company.getName());
+        companyDetailsTextView.setText(details);
     }
 
     public void onHomeClick(View view)
