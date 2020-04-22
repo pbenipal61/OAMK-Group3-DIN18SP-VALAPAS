01b69d281a1f565f751c4001790be0380e8fd7e8
Raul Brumar <raulbrumar@yahoo.de>
2020-04-22 12:01:47 +0300

    Fixed register user

--- app/Valapas/app/src/main/java/com/group3/valapas/RegisterPages/RegisterUser.java
+++ app/Valapas/app/src/main/java/com/group3/valapas/RegisterPages/RegisterUser.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
